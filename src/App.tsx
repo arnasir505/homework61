1f1c38dc468b5af8ee6acf4988d5b508a333926c
Ariet Nasirdinov <ariet.nasirdinov505@gmail.com>
2024-02-01 13:59:46 +0600

    Create function to fetch data, file for constants

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
-import { useState } from 'react';
+import axios from 'axios';
+import { useCallback, useEffect, useState } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [countries, setCountries] = useState([]);
 
-  return <></>;
+  const fetchData = useCallback(async () => {
+    const response = await axios.get('v2/all?fields=alpha3Code,name');
+    const allCountries = response.data.json();
+    setCountries(allCountries);
+  }, []);
+
+  useEffect(() => {
+    fetchData();
+  }, [fetchData]);
+
+  return <div className='container'></div>;
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,9 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import axios from 'axios';
+import { BASE_URL } from './constants';
 
-axios.defaults.baseURL = 'https://restcountries.com/';
+axios.defaults.baseURL = BASE_URL;
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
